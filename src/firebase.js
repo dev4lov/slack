7595f4e37c8f38f0f36f45200f0c479f4a140f39
dev <113306065+dev4lov@users.noreply.github.com>
2023-08-10 00:16:58 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,7 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyChlGIpC79TADC8MzB9yPyIoFa8gPc9CE4",
-  authDomain: "slack-clone-f6e1e.firebaseapp.com",
-  projectId: "slack-clone-f6e1e",
-  storageBucket: "slack-clone-f6e1e.appspot.com",
-  messagingSenderId: "667545817512",
-  appId: "1:667545817512:web:cce5a41f2cd94197ba0d9d",
+  // insert firebase config
 };
 
 const app = initializeApp(firebaseConfig);
